98f67a3260adf3792a5a3a7f1282e1a4983e9743
syed <syedmahmood717@gmail.com>
2026-03-01 00:46:02 +0530

    create add new product and monitor old ones

---
+++ price_tracker/amzn_add_product.py
@@ -0,0 +1,78 @@
+import asyncio
+import json
+from pathlib import Path
+from playwright.async_api import async_playwright
+import datetime
+import random
+from product_schema import product 
+from typing import List
+import sys
+from pathlib import Path
+from dataclasses import asdict
+from pot_schema import pot
+from datetime import datetime
+PROFILE_DIR = Path("/home/syed/Downloads/Workspace/Scripts/profiles/syed")  # Mine was C:/Users/syedm/OneDrive/Desktop/Scripts/profiles/syed
+
+async def main():
+    
+    if len(sys.argv) == 3:
+        product_name= sys.argv[1]
+        product_url= sys.argv[2]
+    else: 
+        product_url="nothing"
+        print("fix your arguments,pal")
+        return
+    async with async_playwright() as p:
+        ctx = await p.chromium.launch_persistent_context(
+            user_data_dir=str(PROFILE_DIR),
+            channel="chrome",        # use installed Chrome
+            headless=False,          # It's better to see the browser in action or it might not work as expected due to not seeing the UI
+            args=["--disable-blink-features=AutomationControlled"],
+            ignore_default_args=["--enable-automation"],
+        )
+    
+        try :
+            page = ctx.pages[0] if ctx.pages else await ctx.new_page()
+
+            await page.goto(product_url, timeout=60_000) 
+
+            await page.wait_for_selector("span.a-price span.a-offscreen")
+            price_str = await page.locator("span.a-price span.a-offscreen").first.text_content()
+            price_str= str(price_str) if price_str is not None else ""
+            price_formatted = float(price_str.replace("₹", "").replace(",", "").strip())
+            print(f"\n Product Name {product_name} Product Price {price_str}\n ")
+            with open("products_list.json") as f:
+                data = json.load(f)
+
+            with open("price_over_time.json") as f:
+                pData= json.load(f)
+
+            product_list = [product(**item) for item in data]
+            pid=len(product_list)
+            product_list.append(product(
+                pid=pid,
+                name=product_name,
+                visual_price=price_str,
+                price=price_formatted,
+                url=product_url
+               ))
+
+            potData= [pot(**item) for item in pData]
+            now=datetime.now()
+            potData.append(pot(uid=len(potData),product_id=pid,price=price_formatted,timestamp=now.isoformat()))
+            print(product_list)
+            with open("products_list.json", "w") as f:
+                json.dump([asdict(p) for p in product_list], f, indent=2,   ensure_ascii=False)
+
+            with open("price_over_time.json", "w") as f:
+                json.dump([asdict(p) for p in potData], f, indent=2,   ensure_ascii=False,)
+
+        except Exception as e:
+            print("This went wrong ",e)
+            return
+        # await asyncio.sleep(1)      
+        await ctx.close()
+ 
+        
+if __name__=="__main__":
+    asyncio.run(main())
--- price_tracker/amzn_prc_monitor.py
+++ price_tracker/amzn_prc_monitor.py
@@ -5,23 +5,26 @@ from pathlib import Path
 from playwright.async_api import async_playwright
 import datetime
 import random
-from price_schema import product 
+from product_schema import product 
 from typing import List
 import sys
 from pathlib import Path
 from dataclasses import asdict
-
+from datetime import datetime
+from pot_schema import pot
 PROFILE_DIR = Path("/home/syed/Downloads/Workspace/Scripts/profiles/syed")  # Mine was C:/Users/syedm/OneDrive/Desktop/Scripts/profiles/syed
 
 async def main():
-    
-    if len(sys.argv) == 3:
-        product_name= sys.argv[1]
-        product_url= sys.argv[2]
-    else: 
-        product_url="nothing"
-        print("fix your arguments,pal")
-        return
+    with open("products_list.json") as f:
+                data = json.load(f)
+
+    with open("price_over_time.json") as f:
+                pData= json.load(f)
+
+    potData= [pot(**item) for item in pData]
+    product_list = [product(**item) for item in data]
+
+    temp_list=product_list.copy()
     async with async_playwright() as p:
         ctx = await p.chromium.launch_persistent_context(
             user_data_dir=str(PROFILE_DIR),
@@ -31,35 +34,47 @@ async def main():
             ignore_default_args=["--enable-automation"],
         )
     
-        try :
-            page = ctx.pages[0] if ctx.pages else await ctx.new_page()
 
-            await page.goto(product_url, timeout=60_000) 
-            await page.wait_for_selector("span.a-offscreen")
-            price_str = await page.locator("span.a-offscreen").first.text_content()
-            price_str= str(price_str) if price_str is not None else ""
-            price_formatted = float(price_str.split("₹")[1])
-            print(f"\n Product Name {product_name} Product Price {price_str}\n ")
-            with open("products_list.json") as f:
-                data = json.load(f)
+        page = ctx.pages[0] if ctx.pages else await ctx.new_page()
+        for item in temp_list:
+            try :
+                await page.goto(item.url, timeout=60_000) 
+                # await page.wait_for_selector("span.a-offscreen")
+                # price_str = await page.locator("span.a-offscreen").first.text_content()
+                
+                await page.wait_for_selector("span.a-price span.a-offscreen")
+                price_str = await page.locator("span.a-price span.a-offscreen").first.text_content()
+                price_str= str(price_str) if price_str is not None else ""
+                price_formatted = float(price_str.replace("₹", "").replace(",", "").strip())
 
-            product_list = [product(**item) for item in data]
+                potData.append(pot(uid=len(potData),product_id=item.pid,price=price_formatted,timestamp=datetime.now().isoformat()))
+                if price_formatted<item.price:
+                    for pr in product_list:
+                        if pr.pid == item.pid:
+                            pr.price=price_formatted 
+                            pr.visual_price=price_str 
 
-            product_list.append(product(
-                id=len(product_list),
-                name=product_name,
-                visual_price=price_str,
-                price=price_formatted,
-                url=product_url
-               ))
-            print(product_list)
-            with open("products_list.json", "w") as f:
-                json.dump([asdict(p) for p in product_list], f, indent=2,   ensure_ascii=False)
+                            break
+                            print("Max Price is less",price_formatted)
 
-        except Exception as e:
-            print("This went wrong ",e)
-            return
-        # await asyncio.sleep(1)      
+                    with open("products_list.json", "w") as f:
+                        json.dump([asdict(p) for p in product_list], f, indent=2,   ensure_ascii=False)
+
+                else:print(f"\n Product Name {item.name} Product Price {item.price} is same\n ")
+
+
+                product_list = [product(**item) for item in data]
+                pid=len(product_list)
+
+
+                with open("price_over_time.json", "w") as f:
+                    json.dump([asdict(p) for p in potData], f, indent=2,   ensure_ascii=False)
+
+
+            except Exception as e:
+                print("This went wrong ",e)
+                return
+            # await asyncio.sleep(1)      
         await ctx.close()
  
         
